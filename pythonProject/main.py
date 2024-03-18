d5ae7a876d6233fc82c27175052f761f5601068f
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-03-18 18:37:18 +0100

    task1

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,7 +1,4 @@
-squares = [x**2 for x in range(1, 11)]
-def generate_squares(start, end):
-    return [x**2 for x in range(start, end)]
+# task1
+squares = [x ** 2 for x in range(1, 11)]
 
 
-squares = generate_squares(1, 11)
-print(squares)
