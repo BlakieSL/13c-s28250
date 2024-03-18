8390e6ee6912486185138e68283c8567247cde62
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-03-18 18:45:36 +0100

    task4

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -13,4 +13,13 @@ class SquareGenerator:
         return [x ** 2 for x in range(start, end)]
 
 
+# task4
+import math
+square_generator = SquareGenerator()
+squares = square_generator.generate_squares(1, 10)
+square_roots = [math.sqrt(x) for x in squares]
+print(squares)
+print(square_roots)
+
+
 
