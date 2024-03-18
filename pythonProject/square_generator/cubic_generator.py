a98d15eb1dd107a5dfa9b1fd0a21d85c592a2c07
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-03-18 19:08:54 +0100

    task9

--- pythonProject/square_generator/cubic_generator.py
+++ pythonProject/square_generator/cubic_generator.py
@@ -4,3 +4,8 @@ from .square_generator import SquareGenerator
 class CubicGenerator(SquareGenerator):
     def generate_cubes(self, start, end):
         return [x ** 3 for x in range(start, end)]
+
+    def generate_squares(self, start, end):
+        if start >= end:
+            raise ValueError("Start of range must be less than the end.")
+        return [x ** 2 for x in range(start, end)]
