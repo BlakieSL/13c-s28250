b82f8eadbe4b61ec49722679f5fbcdd2d687e5cd
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-03-18 13:37:04 +0100

    test

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,6 +1,7 @@
 squares = [x**2 for x in range(1, 11)]
 def generate_squares(start, end):
-     return [x**2 for x in range(start, end)]
+    return [x**2 for x in range(start, end)]
+
 
 squares = generate_squares(1, 11)
 print(squares)
